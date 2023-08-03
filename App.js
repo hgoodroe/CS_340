8b7b7044fa135ad43fc22adc12733bcce6704668
Samantha Affarano <affarans@flip1.engr.oregonstate.edu>
2023-08-02 19:54:37 -0700

    commit

--- App.js
+++ App.js
@@ -9,11 +9,17 @@
 // Setup section- contains variables
 var express = require('express');
 var app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
 var db = require('./database/database_connector');
+
 const { engine } = require('express-handlebars');
 var exphbs = require('express-handlebars');
 app.engine('.hbs', engine({ extname: ".hbs" }));
 app.set('view engine', '.hbs');
+
 PORT = 4020;
 
 
@@ -25,8 +31,24 @@ PORT = 4020;
 
 app.get('/', function (req, res) {
     let allMovies = "SELECT * FROM Movies";                      //Define query (will show all movies)
+    let allSub_Genres = "SELECT * FROM Sub_Genres";
     db.pool.query(allMovies, function (error, rows, fields) {    //Execute the query
-        res.render('index', { data: rows });                     //Render index.hbs file and send data back as rows
+        let movie = rows;
+
+        db.pool.query(allSub_Genres, function (error, rows, fields) {
+            let sub_genre = rows;
+            let sub_genremap = {}
+            sub_genre.map(sub_genre => {
+                let id = parseInt(Sub_Genres.sub_genre_ID, 10);
+                sub_genremap[id] = sub_genre["sub_genre"];
+            })
+
+            movie = movie.map(movie => {
+                return Object.assign(movie, {sub_genre: sub_genremap[movie.sub_genre]})
+            })
+            res.render('index', { data: movie, sub_genre: sub_genre });                  //Render index.hbs file and send data back as rows
+        })
+                             
     })
 });
 
@@ -54,7 +76,24 @@ app.get('', () => {
     }
 });
 
+app.post('/addMembersHasMoviesForm', function (req, res) {
+    //capture data and parse into JS object, can't have NULL values
+    // for Members_Has_Movies add
+    let mhmData = req.body;
 
+    //Create query and run it on the database
+    mhmQueryInsert = `INSERT INTO Members_has_Movies(member_ID, movie_id) VALUES('${data['member_ID_Input']}', '${data['movie_ID_Input']}'`;
+    db.pool.query(mhmQueryInsert, function (error, rows, fields) {
+        if (error) {
+            console.log(error)
+            res.sendStatus(400);
+        }
+        else {
+            res.redirect('/');
+        }
+    })
+
+});
 
 // Listener section- makes server work
 // Note: Don't add or change anything below this line.
