c26a5fcad8c0a6d32ac9bf11ca80ef365e9df011
Matthew Goodroe <goodroem@oregonstate.edu>
2023-08-02 20:22:38 -0700

    Updated to get server working

--- App.js
+++ App.js
@@ -20,83 +20,82 @@ var exphbs = require('express-handlebars');
 app.engine('.hbs', engine({ extname: ".hbs" }));
 app.set('view engine', '.hbs');
 
-PORT = 4020;
+PORT = 4021;
 
 
-//Route section- contains paths server will respond to
-// Testing
-//app.get('/', function (req, res) {
-//    res.send("The server is running!")
-//});
-
+// Route section - contains paths server will respond to Testing
 app.get('/', function (req, res) {
-    let allMovies = "SELECT * FROM Movies";                      //Define query (will show all movies)
-    let allSub_Genres = "SELECT * FROM Sub_Genres";
-    db.pool.query(allMovies, function (error, rows, fields) {    //Execute the query
-        let movie = rows;
-
-        db.pool.query(allSub_Genres, function (error, rows, fields) {
-            let sub_genre = rows;
-            let sub_genremap = {}
-            sub_genre.map(sub_genre => {
-                let id = parseInt(Sub_Genres.sub_genre_ID, 10);
-                sub_genremap[id] = sub_genre["sub_genre"];
-            })
-
-            movie = movie.map(movie => {
-                return Object.assign(movie, {sub_genre: sub_genremap[movie.sub_genre]})
-            })
-            res.render('index', { data: movie, sub_genre: sub_genre });                  //Render index.hbs file and send data back as rows
-        })
-                             
-    })
+    res.send("The server is running for Hunter & Samantha!")
 });
 
-app.get('/', function (req, res) {
-    res.render('index');
-});
+// app.get('/', function (req, res) {
+//     let allMovies = "SELECT * FROM Movies";                      //Define query (will show all movies)
+//     let allSub_Genres = "SELECT * FROM Sub_Genres";
+//     db.pool.query(allMovies, function (error, rows, fields) {    //Execute the query
+//         let movie = rows;
 
-app.get('/requested_Movies_available', async (request, response, next) => {
-    try {
+//         db.pool.query(allSub_Genres, function (error, rows, fields) {
+//             let sub_genre = rows;
+//             let sub_genremap = {}
+//             sub_genre.map(sub_genre => {
+//                 let id = parseInt(Sub_Genres.sub_genre_ID, 10);
+//                 sub_genremap[id] = sub_genre["sub_genre"];
+//             })
 
-    }
-    catch (error) {
-        response.status(500).send(error.message)
-        window.alert("Error Occurred")
-    }
-});
+//             movie = movie.map(movie => {
+//                 return Object.assign(movie, { sub_genre: sub_genremap[movie.sub_genre] })
+//             })
+//             res.render('index', { data: movie, sub_genre: sub_genre });                  //Render index.hbs file and send data back as rows
+//         })
 
-app.get('', () => {
-    try {
+//     })
+// });
 
-    }
-    catch (error) {
-        response.status(500).send(error.message)
-        window.alert("Error Occurred")
-    }
-});
+// app.get('/', function (req, res) {
+//     res.render('index');
+// });
 
-app.post('/addMembersHasMoviesForm', function (req, res) {
-    //capture data and parse into JS object, can't have NULL values
-    // for Members_Has_Movies add
-    let mhmData = req.body;
+// app.get('/requested_Movies_available', async (request, response, next) => {
+//     try {
 
-    //Create query and run it on the database
-    mhmQueryInsert = `INSERT INTO Members_has_Movies(member_ID, movie_id) VALUES('${data['member_ID_Input']}', '${data['movie_ID_Input']}'`;
-    db.pool.query(mhmQueryInsert, function (error, rows, fields) {
-        if (error) {
-            console.log(error)
-            res.sendStatus(400);
-        }
-        else {
-            res.redirect('/');
-        }
-    })
+//     }
+//     catch (error) {
+//         response.status(500).send(error.message)
+//         window.alert("Error Occurred")
+//     }
+// });
 
-});
+// app.get('', () => {
+//     try {
+
+//     }
+//     catch (error) {
+//         response.status(500).send(error.message)
+//         window.alert("Error Occurred")
+//     }
+// });
+
+// app.post('/addMembersHasMoviesForm', function (req, res) {
+//     //capture data and parse into JS object, can't have NULL values
+//     // for Members_Has_Movies add
+//     let mhmData = req.body;
+
+//     //Create query and run it on the database
+//     mhmQueryInsert = `INSERT INTO Members_has_Movies(member_ID, movie_id) VALUES('${data['member_ID_Input']}', '${data['movie_ID_Input']}'`;
+//     db.pool.query(mhmQueryInsert, function (error, rows, fields) {
+//         if (error) {
+//             console.log(error)
+//             res.sendStatus(400);
+//         }
+//         else {
+//             res.redirect('/');
+//         }
+//     })
+
+// });
 
 // Listener section- makes server work
 // Note: Don't add or change anything below this line.
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}...`);
+    console.log(`Server is working for Hunter & Samantha ${PORT}...`);
 });
