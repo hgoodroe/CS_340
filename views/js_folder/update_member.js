c472d982d85864015c888713f0db52f94f6bb06c
Matthew Goodroe <goodroem@oregonstate.edu>
2023-08-09 22:03:53 -0700

    updated movies

--- views/js_folder/update_member.js
+++ views/js_folder/update_member.js
@@ -1,41 +1,34 @@
 // Get the objects we need to modify
-let updateMovieForm = document.getElementById('update-movie-form');
+let updateMemberForm = document.getElementById('update-member-form-ajax');
 
 // Modify the objects we need
-updateMovieForm.addEventListener("submit", function (e) {
+updateMemberForm.addEventListener("submit", function (e) {
 
     // Prevent the form from submitting
     e.preventDefault();
 
     // Get form fields we need to get data from
-    let inputMovieName = document.getElementById("mySelect");
-    let inputSubGenre = document.getElementById("input-sub_genre-update");
-    console.log(inputMovieName)
+    let inputName = document.getElementById("mySelect");
+    let inputEmail = document.getElementById("input-email-update");
+    let inputAddress = document.getElementById("input-address-update");
 
     // Get the values from the form fields
-    let movieNameValue = inputMovieName.value;
-    let sub_GenreValue = inputSubGenre.value;
-    console.log(movieNameValue)
-    console.log(sub_GenreValue)
-
-    // currently the database table for bsg_people does not allow updating values to NULL
-    // so we must abort if being bassed NULL for homeworld
-
-    if (isNaN(sub_GenreValue)) {
-        console.log("Error here - sub_GenreValue is NAN")
-        return;
-    }
+    let nameValue = inputName.value;
+    let newEmail = inputEmail.value;
+    let newAddress = inputAddress.value;
 
 
     // Put our data we want to send in a javascript object
     let data = {
-        movie_id: movieNameValue,
-        sub_genre_ID: sub_GenreValue,
+        name: nameValue,
+        email: newEmail,
+        address: newAddress
+
     }
 
     // Setup our AJAX request
     var xhttp = new XMLHttpRequest();
-    xhttp.open("PUT", '/put-movie-ajax', true);
+    xhttp.open("PUT", "/put-member-ajax/", true);
     xhttp.setRequestHeader("Content-type", "application/json");
 
     // Tell our AJAX request how to resolve
@@ -43,7 +36,7 @@ updateMovieForm.addEventListener("submit", function (e) {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
 
             // Add the new data to the table
-            updateRow(xhttp.response, sub_GenreValue);
+            updateRow(xhttp.response, nameValue);
 
         }
         else if (xhttp.readyState == 4 && xhttp.status != 200) {
@@ -57,24 +50,24 @@ updateMovieForm.addEventListener("submit", function (e) {
 })
 
 
-function updateRow(data, movieNameValue) {
+function updateRow(data, member_ID) {
     let parsedData = JSON.parse(data);
 
-    let table = document.getElementById("movieTable");
+    let table = document.getElementById("memberTable");
 
     for (let i = 0, row; row = table.rows[i]; i++) {
         //iterate through rows
         //rows would be accessed using the "row" variable assigned in the for loop
-        if (table.rows[i].getAttribute("data-value") == movieNameValue) {
+        if (table.rows[i].getAttribute("data-value") == member_ID) {
 
             // Get the location of the row where we found the matching person ID
             let updateRowIndex = table.getElementsByTagName("tr")[i];
 
             // Get td of homeworld value
-            let td = updateRowIndex.getElementsByTagName("td")[7];
+            let td = updateRowIndex.getElementsByTagName("td")[3];
 
             // Reassign homeworld to our value we updated to
-            td.innerHTML = parsedData[0].sub_genre_ID;
+            td.innerHTML = parsedData[0].name;
         }
     }
 }
