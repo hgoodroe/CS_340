1bd906b5b18d150838b13b69a6b27d4141a264a2
Samantha Affarano <affarans@flip1.engr.oregonstate.edu>
2023-08-14 22:46:21 -0700

    commit

--- database/database_connector.js
+++ database/database_connector.js
@@ -9,9 +9,9 @@ var mysql = require('mysql')
 var pool = mysql.createPool({
     connectionLimit: 10,
     host: 'classmysql.engr.oregonstate.edu',
-    user: 'cs340_goodroem',
-    password: '2807',
-    database: "cs340_goodroem"
+    user: '*****',
+    password: '****',
+    database: "******"
 })
 
 module.exports.pool = pool;
